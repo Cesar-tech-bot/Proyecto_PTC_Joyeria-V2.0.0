73378f09a8f433d5ba6e97376caaae95030b7e9f
Davidsillo273 <20240273@ricaldone.edu.sv>
2024-08-20 22:50:06 -0600

    He creado el sistema de Carpetas

--- SistemaJoyería/Program.cs
+++ SistemaJoyería/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SistemaJoyería.View.LoginView;
 
 namespace SistemaJoyería
 {
@@ -16,7 +17,7 @@ namespace SistemaJoyería
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmLogin());
         }
     }
 }
