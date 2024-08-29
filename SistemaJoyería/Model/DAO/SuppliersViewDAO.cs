d59cef089072f0c6acd237ed9f06e0712dfda0e7
Leoo091 <leoabarca091@gmail.com>
2024-08-28 18:45:48 -0600

    Codificacion del FrmSupplier

--- SistemaJoyería/Model/DAO/SuppliersViewDAO.cs
+++ SistemaJoyería/Model/DAO/SuppliersViewDAO.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using SistemaJoyeria.DTO;
 
-namespace SistemaJoyería.Model.DAO
+namespace SistemaJoyeria.DAO
 {
-    internal class SuppliersView
+    public class SupplierDAO
     {
+        private string connectionString = "Your_Connection_String_Here";
+
+        public void Add(SupplierDTO supplier)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Suppliers (NombreEmpresa, NombreContacto, Telefono, Email, Direccion) VALUES (@NombreEmpresa, @NombreContacto, @Telefono, @Email, @Direccion)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@NombreEmpresa", supplier.NombreEmpresa);
+                command.Parameters.AddWithValue("@NombreContacto", supplier.NombreContacto);
+                command.Parameters.AddWithValue("@Telefono", supplier.Telefono);
+                command.Parameters.AddWithValue("@Email", supplier.Email);
+                command.Parameters.AddWithValue("@Direccion", supplier.Direccion);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public List<SupplierDTO> Search(string searchTerm)
+        {
+            List<SupplierDTO> results = new List<SupplierDTO>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Suppliers WHERE NombreEmpresa LIKE @SearchTerm OR ID = @ID";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@SearchTerm", "%" + searchTerm + "%");
+                command.Parameters.AddWithValue("@ID", int.TryParse(searchTerm, out int id) ? id : (object)DBNull.Value);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    results.Add(new SupplierDTO
+                    {
+                        ID = Convert.ToInt32(reader["ID"]),
+                        NombreEmpresa = reader["NombreEmpresa"].ToString(),
+                        NombreContacto = reader["NombreContacto"].ToString(),
+                        Telefono = reader["Telefono"].ToString(),
+                        Email = reader["Email"].ToString(),
+                        Direccion = reader["Direccion"].ToString()
+                    });
+                }
+            }
+            return results;
+        }
     }
 }
