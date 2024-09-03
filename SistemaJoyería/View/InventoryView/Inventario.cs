33f22fb6636b624518ac4c5e8f77ac216a4edcae
Leoo091 <leoabarca091@gmail.com>
2024-09-03 16:57:16 -0600

    quite inventario

--- SistemaJoyería/View/InventoryView/Inventario.cs
+++ SistemaJoyería/View/InventoryView/Inventario.cs
@@ -1,4 +1,5 @@
 ﻿using SistemaJoyería.Controller.Inventario;
+using SistemaJoyería.Controller.Suppliers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,7 +17,7 @@ namespace SistemaJoyería.View.InventarioView
         public Inventario()
         {
             InitializeComponent();
-            Inventario obj = new Inventario(this);
+            new Inventario(this);
         }
     }
 }
