0633d2257979a13fec10dd8c548ae2fec075d87e
Leoo091 <leoabarca091@gmail.com>
2024-09-04 18:58:03 -0600

    dbcontext

--- SistemaJoyería/Model/dbContext.cs
+++ SistemaJoyería/Model/dbContext.cs
@@ -16,7 +16,7 @@ namespace SistemaJoyería.Model
                 try
                 {
                 //Defininmos el server
-                string server = "LAPTOP-MU6BRPB9\\SQLEXPRESS";
+                string server = "";
                 //Ahora la DB
                 string database = "DB_CRUD";
                 //Creamos la conexión 
