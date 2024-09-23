952d094627ac77a58dcd354c58c01ad0e6499f4c
Davidsillo273 <20240273@ricaldone.edu.sv>
2024-09-22 21:14:52 -0600

    Se arreglo lo de correo

--- SistemaJoyería/View/LoginView/RecuperarCorreo.cs
+++ SistemaJoyería/View/LoginView/RecuperarCorreo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaJoyería.Controller.LoginController;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,7 @@ namespace SistemaJoyería.View.LoginView
         public RecuperarCorreo()
         {
             InitializeComponent();
+            ControllerRecuperarPorCorreo control = new ControllerRecuperarPorCorreo(this);
         }
     }
 }
